e60e8160b18928414de26ff7848a6528f5a19d11
Varit J Patel <varitpatel@gmail.com>
2021-11-15 10:37:37 +0530

    chore: Add custom control to the video and extend feedback functionlity

--- src/app/components/feedback-list/feedback-list.component.ts
+++ src/app/components/feedback-list/feedback-list.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, SimpleChanges } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Feedback } from 'src/app/models/feedback.model';
+import { FeedbackService } from 'src/app/services/feedback/feedback.service';
 
 @Component({
   selector: 'app-feedback-list',
@@ -9,9 +11,39 @@ import { Feedback } from 'src/app/models/feedback.model';
 })
 export class FeedbackListComponent {
   @Input() feedbacks!: Array<Feedback>;
+  @Input() selectedFeedback!: Feedback;
+
+  reply = new FormControl('', [Validators.required]);
+
+  constructor(private readonly feedbackService: FeedbackService) {}
 
   // Track each feedback by time which is the unique property in the list of feedbacks
   trackByFeedback(_index: number, feedback: Feedback): number {
     return feedback.time;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    if(changes?.feedbacks?.currentValue) {
+      this.feedbacks = changes.feedbacks.currentValue
+    }
+
+    if(changes?.selectedFeedback?.currentValue) {
+      this.selectedFeedback = changes.selectedFeedback.currentValue
+    }
+  }
+
+  sendReply(feedback: Feedback): void {
+    console.log('this.feedback', this.reply)
+    if(!this.reply.valid) {
+      return;
+    }
+    this.feedbackService.sendReply(feedback, this.reply.value);
+    this.reply.setValue('');
+  }
+
+  markResolved(feedback: Feedback): void {
+    this.feedbackService.markFeedbackResolved(feedback);
   }
 }
